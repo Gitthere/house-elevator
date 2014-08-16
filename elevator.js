e0d0542a6cc636cbcb6c6d438741a8a0c9f4def4
Gitthere <ichi4g@gmail.com>
2014-08-16 12:04:39 -1000

    all tests passed

--- elevator.js
+++ elevator.js
@@ -7,6 +7,12 @@ module.exports = {
     if (selectedFloor > 3 || (selectedFloor-currentFloor) < -3) {
       return 0;
     }
+    // console.log(typeof currentFloor);
+    // console.log(typeof selectedFloor);
+
+    if (typeof currentFloor !== 'number' || typeof selectedFloor !== 'string') {
+      return 0;
+    }
 
     if (selectedFloor < currentFloor) {
       return parseInt(selectedFloor-currentFloor);
